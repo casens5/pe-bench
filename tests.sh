c8a5fd9e4d4690f2563a7f7ce1d6b4755c22b0df
Michael Peter Christen <mc@yacy.net>
2025-01-07 19:21:08 +0100

    fix

---
+++ tests.sh
@@ -0,0 +1,14 @@
+python3 inference.py --endpoint OpenAI_GPT-4o-Mini --skip_existing --nall
+python3 inference.py --endpoint OpenAI_GPT-4o --skip_existing --nall
+
+python3 inference.py --endpoint OpenAI_GPT-4o-Mini --skip_existing --nall --language java
+python3 inference.py --endpoint OpenAI_GPT-4o --skip_existing --nall --language java
+
+python3 inference.py --endpoint OpenAI_GPT-4o-Mini --skip_existing --nall --language clojure
+python3 inference.py --endpoint OpenAI_GPT-4o --skip_existing --nall --language clojure
+
+#python3 test.py --endpoint OpenAI_GPT-o1-Mini --skip_existing
+#python3 test.py --endpoint OpenAI_GPT-o1 --skip_existing
+
+#python3 test.py --endpoint OpenAI_GPT-o1-Mini --skip_existing --language java
+#python3 test.py --endpoint OpenAI_GPT-o1 --skip_existing --language java
